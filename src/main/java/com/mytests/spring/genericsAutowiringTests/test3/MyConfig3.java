29a29c856ec52cb13a41dbc17d90e60d0f6fabdc
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-01-21 17:39:08 +0300

    List<Type> autowiring in case of no beans of specified type: constructor injection - should be no errors for constructor injection and some error for field injection

--- src/main/java/com/mytests/spring/genericsAutowiringTests/test3/MyConfig3.java
+++ src/main/java/com/mytests/spring/genericsAutowiringTests/test3/MyConfig3.java
@@ -1,7 +1,11 @@
 package com.mytests.spring.genericsAutowiringTests.test3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 /**
  * *
@@ -12,6 +16,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class MyConfig3 {
-    
-    
+
+    @Bean
+    Foo foo(List<Bar> bars)    // no autowiring error - ok
+    {
+        return new Foo();
+    }
+    @Bean 
+    Boo boo(){
+        return new Boo();
+    }
+    static class Foo {}
+    static class Bar {}
+    static class Boo {
+        
+        @Autowired(required = false) List<Bar> bars;   // no autowiring error if required is set to true - incorrect!
+    }
 }
